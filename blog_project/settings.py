922b95d1e025e565a99b9872a40be23a3249f2e8
andreytp <andreytp@gmail.com>
2021-04-19 19:57:52 +0300

    add login logoff

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -89,6 +89,8 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
